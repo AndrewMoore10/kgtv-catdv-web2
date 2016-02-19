b9bd37df92446e322a08a37a1812de009bf61644
exit <exit>
2016-02-19 08:46:58 -0800

    troublshooting firefox

--- js/logic/DownloadTracker.js
+++ js/logic/DownloadTracker.js
@@ -19,12 +19,12 @@ var downloadTracker = function(){
   var functions = {
     updateDownloadCount : function(clipID, cb, fcb){
       $catdv.getClip(clipID, function(result){
+        console.log(result);
         var downloadCount = (result.userFields && result.userFields.U12 ? parseInt(result.userFields.U12) : 0);
         var downloadBy = (result.userFields && result.userFields.U13 ? result.userFields.U13 : "");
         result.userFields.U12 = downloadCount + 1;
         result.userFields.U13 = downloadBy + "<p>" + $.cookie("username") + " @ " + moment().format('MMMM Do YYYY, h:mm:ss a Z z') + "</p>\n";
         $catdv.saveClip(result, cb, fcb);
-        console.log(result);
         // cb();
       }, fcb);
     }
@@ -35,16 +35,17 @@ var tracker = downloadTracker();
 
 $(document).on('click', '.field_MF a', function(event){
   // alert("Download clicked! " + getUrlParameter("id"));
-  console.log(tracker);
-  console.log(event);
+  // console.log(tracker);
+  // console.log(event);
   tracker.updateDownloadCount(getUrlParameter("id"),
     function(){
-      // console.log("success"); 
+      console.log("success"); 
     },
     function(){ 
-      // console.log("failed"); 
+      console.log("failed"); 
     }
   )  
+  return true;
 })
 
 var getUrlParameter = function getUrlParameter(sParam) {
